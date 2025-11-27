0bb0f6e4f9509e48f58bcdcd6abe196c4e9b7690
truongnam <tgknam2005@gmail.com>
2025-11-27 15:05:46 +0700

    feat: list locations

--- scripts/procedures/location.sql
+++ scripts/procedures/location.sql
@@ -67,4 +67,78 @@ BEGIN
     WHERE location_id = p_LocationId;
 END //
 
+CREATE PROCEDURE GetOwnerInventory (
+    IN p_owner_id BINARY(16)
+)
+BEGIN
+    SELECT 
+        -- Location Details
+        BIN_TO_UUID(l.location_id) AS location_id,
+        l.name AS location_name,
+        l.addrNo, 
+        l.ward, 
+        l.city,
+        l.avgRating,
+        l.isActive AS isLocationActive,
+        
+        -- Venue Details
+        BIN_TO_UUID(v.venueType_id) as venueType_id,
+        v.name AS venue_name,
+        v.floor,
+        v.pricePerHour,
+        v.isActive AS isVenueActive
+        
+    FROM locations l
+    LEFT JOIN venues v ON l.location_id = v.location_id
+    WHERE l.owner_id = p_owner_id
+    ORDER BY l.createdAt DESC, v.name ASC;
+END //
+
+CREATE PROCEDURE GetOwnerOrders (
+    IN p_owner_id BINARY(16),
+    IN p_status VARCHAR(20),  -- Pass NULL to see ALL orders
+    IN p_page INT,
+    IN p_limit INT
+)
+BEGIN
+    DECLARE v_offset INT;
+    
+    -- Pagination Logic
+    SET p_page = IF(p_page < 1, 1, p_page);
+    SET p_limit = IF(p_limit < 1, 10, p_limit);
+    SET v_offset = (p_page - 1) * p_limit;
+
+    SELECT 
+        BIN_TO_UUID(o.order_id) AS order_id,
+        o.status,
+        o.totalPrice,
+        o.startHour,
+        o.endHour,
+        o.createdAt AS bookingDate,
+        o.points,
+        
+        -- Venue Details
+        v.name AS venue_name,
+        v.floor,
+        
+        -- Location Details
+        l.name AS location_name,
+        l.city,
+        l.addrNo,
+        
+        -- Client ID
+        BIN_TO_UUID(o.client_id) AS client_id
+
+    FROM orders o
+    INNER JOIN venues v ON o.venue_loc_id = v.location_id AND o.venueName = v.name
+    INNER JOIN locations l ON v.location_id = l.location_id
+    
+    WHERE l.owner_id = p_owner_id
+    -- Standard Filter: If p_status is NULL, ignore this check. If not NULL, check equality.
+    AND (p_status IS NULL OR o.status = p_status)
+    
+    ORDER BY o.createdAt DESC
+    LIMIT p_limit OFFSET v_offset;
+END //
+
 DELIMITER ;
