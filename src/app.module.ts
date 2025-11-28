1c05e514766d65b231dd631ad3f434d70f7f3dd1
truongnam <tgknam2005@gmail.com>
2025-11-28 16:10:20 +0700

    feat: order and payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { commonConfig } from './config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { VenueModule } from './venue/venue.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -18,6 +20,8 @@ import { VenueModule } from './venue/venue.module';
     AuthModule,
     UserModule,
     VenueModule,
+    OrderModule,
+    PaymentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
