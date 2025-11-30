7848484b2027891db8698b452a6d85a8dd2d2021
GitGud031005 <nguyenphuc102005@gmail.com>
2025-11-30 19:46:54 +0700

    fix: final select venue

---
+++ scripts/TRIGGER/CONSTRAINT_UPON/location_con.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+CREATE TRIGGER Check_ExistOrderLoc
+BEFORE DELETE ON locations
+FOR EACH ROW
+BEGIN
+    -- Logic: Kiểm tra xem có bất kỳ Order nào thuộc Location này
+    -- có trạng thái là PENDING, CONFIRMED hoặc COMPLETED hay không.
+    -- (Lưu ý: Bảng ORDERS có cột venue_loc_id chính là location_id nên không cần join bảng VENUES)
+    
+    IF EXISTS (
+        SELECT 1 
+        FROM orders o
+        WHERE o.venue_loc_id = OLD.location_id
+          AND o.status IN ('PENDING', 'CONFIRMED', 'COMPLETED')
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Error: Cannot delete Location. It contains venues with associated booking history (Pending, Confirmed, or Completed). Please deactivate (Soft Delete) instead.';
+    END IF;
+END$$
+
+DELIMITER ;
---
+++ scripts/TRIGGER/CONSTRAINT_UPON/venue_con.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+CREATE TRIGGER Check_ExistOrderVen
+BEFORE DELETE ON venues
+FOR EACH ROW
+BEGIN
+    -- Logic: Kiểm tra xem phòng này có bất kỳ đơn hàng nào (kể cả đã hoàn thành)
+    -- Nếu có -> Báo lỗi, không cho xóa.
+    IF EXISTS (
+        SELECT 1 
+        FROM orders o
+        WHERE o.venue_loc_id = OLD.location_id 
+          AND o.venueName = OLD.name
+          AND o.status IN ('PENDING', 'CONFIRMED', 'COMPLETED')
+    ) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Error: Cannot delete Venue. It has associated bookings history (Pending, Confirmed, or Completed). Please deactivate it instead.';
+    END IF;
+END$$
+
+DELIMITER ;
