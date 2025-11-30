ab1afe3123c722a5b859b21db7143329e024384a
GitGud031005 <nguyenphuc102005@gmail.com>
2025-11-30 21:30:31 +0700

    feat: calc discount price

--- scripts/TRIGGER/DERIVED_UPON/order_amenity.sql
+++ scripts/TRIGGER/DERIVED_UPON/order_amenity.sql
@@ -4,12 +4,12 @@ AFTER INSERT ON order_amenities
 FOR EACH ROW
 BEGIN
     -- Cập nhật bảng ORDERS: Cộng thêm giá của tiện nghi vừa thêm
-    UPDATE orders
-    SET 
-        totalPrice = totalPrice + NEW.price,
-        points = FLOOR((totalPrice + NEW.price) / 10000) -- Tính lại điểm theo tổng tiền mới
-    WHERE order_id = NEW.order_id;
-    
+    -- UPDATE orders
+    -- SET 
+    --     totalPrice = totalPrice + NEW.price,
+    --     points = FLOOR((totalPrice + NEW.price) / 10000) -- Tính lại điểm theo tổng tiền mới
+    -- WHERE order_id = NEW.order_id;
+    CALL Cal_Final_Price(NEW.order_id);
     -- (Logic cũ: Khóa Amenity)
     UPDATE amenities SET isActive = 0 WHERE amenity_id = NEW.amenity_id;
 END$$
@@ -22,12 +22,12 @@ AFTER DELETE ON order_amenities
 FOR EACH ROW
 BEGIN
     -- Cập nhật bảng ORDERS: Trừ đi giá của tiện nghi vừa xóa
-    UPDATE orders
-    SET 
-        totalPrice = totalPrice - OLD.price,
-        points = FLOOR((totalPrice - OLD.price) / 10000)
-    WHERE order_id = OLD.order_id;
-
+    -- UPDATE orders
+    -- SET 
+    --     totalPrice = totalPrice - OLD.price,
+    --     points = FLOOR((totalPrice - OLD.price) / 10000)
+    -- WHERE order_id = OLD.order_id;
+    CALL Cal_Final_Price(OLD.order_id);
     -- (Logic cũ: Mở khóa Amenity)
     UPDATE amenities SET isActive = 1 WHERE amenity_id = OLD.amenity_id;
 END$$
