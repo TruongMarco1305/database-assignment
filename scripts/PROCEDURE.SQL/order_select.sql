acd950ab8fcef7a0029d697158136ff49bec7332
Truong Tri Hao <hao.truongmywtag@hcmut.edu.vn>
2025-12-03 13:59:07 +0700

    feat: add procedures

--- scripts/PROCEDURE.SQL/order_select.sql
+++ scripts/PROCEDURE.SQL/order_select.sql
@@ -47,7 +47,7 @@ BEGIN
 END //
 
 DELIMITER $$
-CREATE PROCEDURE getIncomingOrdersForOwner(
+CREATE PROCEDURE getOrdersForOwner(
     IN p_ownerId BINARY(16)
 )
 BEGIN
@@ -57,41 +57,16 @@ BEGIN
     o.startTime,
     o.endTime,
     o.totalPrice,
-    BIN_TO_UUID(c.user_id) AS client_id,
     c.firstName AS client_first_name,
     c.lastName AS client_last_name,
-    c.email AS client_email,
-    c.phoneNo AS client_phone
+    c.phoneNo AS client_phone,
+    i.senderBankAccount AS client_bank_account,
+    i.paidOn AS payment_date
   FROM orders o
   JOIN clients c ON o.client_id = c.user_id
   JOIN venues v ON o.venue_id = v.venue_id
+  JOIN invoices i ON o.order_id = i.order_id
   WHERE v.owner_id = p_ownerId
-    AND o.status IN ('PENDING', 'CONFIRMED')  -- Only incoming orders
-  ORDER BY o.startTime DESC;
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE getCompletedOrdersForOwner(
-    IN p_ownerId BINARY(16)
-)
-BEGIN
-  SELECT
-    BIN_TO_UUID(o.order_id) AS order_id,
-    o.status AS order_status,
-    o.startTime,
-    o.endTime,
-    o.totalPrice,
-    BIN_TO_UUID(c.user_id) AS client_id,
-    c.firstName AS client_first_name,
-    c.lastName AS client_last_name,
-    c.email AS client_email,
-    c.phoneNo AS client_phone
-  FROM orders o
-  JOIN clients c ON o.client_id = c.user_id
-  JOIN venues v ON o.venue_id = v.venue_id
-  WHERE v.owner_id = p_ownerId
-    AND o.status = 'COMPLETED'  -- Only incoming orders
   ORDER BY o.startTime DESC;
 END$$
 DELIMITER ;
